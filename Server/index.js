581659f82bd34e23cb1b9efd271af0391c1ed8a8
SnappyKODE <kartikeykhanna461@gmail.com>
2024-02-22 18:18:55 +0530

    commit

---
+++ Server/index.js
@@ -0,0 +1,49 @@
+//creating server
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer((req,res)=>{
+    if (req.url !== '/') {
+        res.writeHead(404);
+        res.end('Not found');
+        return;
+    }
+
+    res.writeHead(200, {
+        'Content-Type': 'text',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET',
+    });
+    res.end();
+});
+
+//socket server
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        },
+});
+
+//socket work
+io.on("connection", (socket) => {
+
+    let users = {}
+
+    socket.on('new-user-joined',(name)=>{
+        users[socket.id] =name;
+        socket.broadcast.emit('user-joined',name);
+    })
+
+    socket.on('send',data=>{
+        socket.broadcast.emit('recieve',{message:data.msg,name:data.user});
+    })
+
+    socket.on('disconnect',name=>{
+        socket.broadcast.emit('leave',users[socket.id]);
+        delete users[socket.id];
+    })
+});
+
+//listening to server
+httpServer.listen(8000);
